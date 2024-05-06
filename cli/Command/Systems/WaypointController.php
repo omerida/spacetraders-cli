f9d31adea11237a4b3d9440f47478c5e1eed6158
Oscar Merida <oscar@oscarm.org>
2024-05-05 23:06:08 -0400

    Adds a waypoint symbol value object.

--- cli/Command/Systems/WaypointController.php
+++ cli/Command/Systems/WaypointController.php
@@ -2,43 +2,29 @@
 
 namespace Phparch\SpaceTradersCLI\Command\Systems;
 
-use CuyZ\Valinor\MapperBuilder;
-use CuyZ\Valinor\Normalizer\Format;
 use Minicli\Command\CommandController;
 use Phparch\SpaceTraders\Client;
 use Phparch\SpaceTraders\ServiceContainer;
 use Phparch\SpaceTraders\Trait\TerminalOutputHelper;
+use Phparch\SpaceTraders\Value\WaypointSymbol;
 
 class WaypointController extends CommandController
 {
     use TerminalOutputHelper;
 
     public function required(): array {
-        return ['system', 'waypoint'];
+        return ['waypoint'];
     }
 
     public function handle(): void
     {
         $client = ServiceContainer::get(Client::class);
 
-
         try {
-            $system = $this->getParam('system');
-            $waypoint = $this->getParam('waypoint');
+            $input = $this->getParam('waypoint');
+            $wp = new WaypointSymbol($input);
 
-            // @todo system regexp can be stricter, maybe a value object?
-            if (!preg_match('/[[:alnum:]-]+/', $system)) {
-                throw new \InvalidArgumentException("Symbol can only include letters, numbers, underscores");
-            }
-
-            // @todo waypoint regexp can be stricter, maybe a value object?
-            if (!preg_match('/[[:alnum:]-]+/', $waypoint)) {
-                throw new \InvalidArgumentException("Faction can only include letters, numbers, underscores");
-            }
-
-            // get a value object from json
-            $response = $client->systemLocation($system, $waypoint);
-
+            $response = $client->systemLocation($wp->system, $wp->waypoint);
             $this->outputVar($response);
 
         } catch (\Throwable $e) {
