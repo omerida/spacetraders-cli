f99953d6796b74d7ee12b2b138bedd62bbab85e1
Oscar Merida <oscar@oscarm.org>
2024-09-18 00:03:24 -0400

    Add command and client for purchase a ship. Render ship list

---
+++ cli/Command/Fleet/PurchaseShipController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Phparch\SpaceTradersCLI\Command\Fleet;
+
+use Minicli\Command\CommandController;
+use Phparch\SpaceTraders\Client;
+use Phparch\SpaceTraders\ServiceContainer;
+use Phparch\SpaceTraders\Value\WaypointSymbol;
+use Phparch\SpaceTradersCLI\Command\HelpInfo;
+
+#[HelpInfo(description: "Purchase a type of ship from a shipyard at a waypoint.", params: ['waypoint symbol', 'ship type'])]
+class PurchaseShipController extends CommandController {
+    public function handle(): void
+    {
+        $client = ServiceContainer::get(Client\Fleet::class);
+
+        $args = $this->getArgs();
+        $waypoint = $args[3] ?? null;
+        if (!$waypoint) {
+            throw new \InvalidArgumentException("Please specify waypoint as third parameter");
+        }
+
+        $waypoint = new WaypointSymbol($waypoint);
+        $type = strtoupper($args[4]) ?? null;
+        if (!$type) {
+            throw new \InvalidArgumentException("Please specify type as fourth parameter");
+        }
+
+        /* @TODO validate type is one of the allowed values */
+        $response = $client->purchaseShip($waypoint, $type);
+
+        var_dump($response);
+    }
+}
---
+++ cli/Render/Ship.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace Phparch\SpaceTradersCLI\Render;
+
+use \Phparch\SpaceTraders\Value;
+
+class Ship extends AbstractRenderer {
+        public function __construct(
+        public Value\Ship $ship,
+    ) {
+    }
+
+    public function output(): string
+    {
+        $this->heading($this->ship->symbol);
+        $this->sprintf(
+            '<:CYN:>## NAV<:DEF:> %s (%s)',
+            $this->ship->nav->systemSymbol,
+            $this->ship->nav->waypointSymbol,
+        );
+        /* @todo render a route */
+        $this->sprintf(
+            '<:YEL:>Status<:DEF:> %s  <:YEL:>Flight Mode<:DEF:> %s  ',
+            $this->ship->nav->status,
+            $this->ship->nav->flightMode,
+        );
+
+        /* @todo render Crew Info */
+        // FRAME
+        $this->sprintf(
+            '<:CYN:>## FRAME<:DEF:> %s (%s)',
+            $this->ship->frame->name,
+            $this->ship->frame->symbol,
+        );
+        $this->writeln($this->green(wordwrap($this->ship->frame->description, 80)));
+        $this->sprintf(
+            '<:YEL:>Condition<:DEF:>       %3s  <:YEL:>Integrity<:DEF:>     %3s  <:YEL:>Module Slots<:DEF:> %s',
+            $this->ship->frame->condition,
+            $this->ship->frame->integrity,
+            $this->ship->frame->moduleSlots,
+        );
+        $this->sprintf(
+            '<:YEL:>Mounting Points<:DEF:> %3s  <:YEL:>Fuel Capacity<:DEF:> %3s',
+            $this->ship->frame->mountingPoints,
+            $this->ship->frame->fuelCapacity,
+        );
+        $this->sprintf(
+            '<:MAG:>Power<:DEF:> %3s  <:MAG:>Crew<:DEF:> %3s  <:MAG:>Slots<:DEF:> %3s',
+            $this->ship->frame->requirements->power,
+            $this->ship->frame->requirements->crew,
+            $this->ship->frame->requirements->slots,
+        );
+        $this->newline();
+        // ENGINE
+        $this->sprintf(
+            '<:CYN:>## ENGINE<:DEF:> %s (%s)',
+            $this->ship->engine->name,
+            $this->ship->engine->symbol,
+        );
+        $this->writeln($this->green(wordwrap($this->ship->engine->description, 80)));
+        $this->sprintf(
+            '<:YEL:>Speed<:DEF:> %3s  <:YEL:>Condition<:DEF:> %s  <:YEL:>Integrity<:DEF:> %s',
+            $this->ship->engine->speed,
+            $this->ship->engine->condition,
+            $this->ship->engine->integrity,
+        );
+        $this->sprintf(
+            '<:MAG:>Power<:DEF:> %3s  <:MAG:>Crew<:DEF:> %3s  <:MAG:>Slots<:DEF:> %3s',
+            $this->ship->engine->requirements->power,
+            $this->ship->engine->requirements->crew,
+            $this->ship->engine->requirements->slots ?? '-',
+        );
+        $this->newline();
+        // REACTOR
+        $this->sprintf(
+            '<:CYN:>## REACTOR<:DEF:> %s (%s)',
+            $this->ship->reactor->name,
+            $this->ship->reactor->symbol,
+        );
+        $this->writeln($this->green(wordwrap($this->ship->reactor->description, 80)));
+
+        $this->sprintf(
+            '<:YEL:>Power Output<:DEF:> %3s  <:YEL:>Condition<:DEF:> %s  <:YEL:>Integrity<:DEF:> %s',
+            $this->ship->reactor->powerOutput,
+            $this->ship->reactor->condition,
+            $this->ship->reactor->integrity,
+        );
+
+        $this->sprintf(
+            '<:MAG:>Power<:DEF:> %3s  <:MAG:>Crew<:DEF:> %3s  <:MAG:>Slots<:DEF:> %3s',
+            $this->ship->reactor->requirements->power ?? '-',
+            $this->ship->reactor->requirements->crew ?? '-',
+            $this->ship->reactor->requirements->slots ?? '-',
+        );
+        $this->newline();
+        $this->writeln($this->cyan('## MOUNTS'));
+
+        if ($this->ship->mounts) {
+            foreach ($this->ship->mounts as $i => $mount) {
+                $this->sprintf(
+                    '%d. %s <:YEL:>(%s)',
+                    $i + 1,
+                    $mount->name,
+                    $mount->symbol,
+                );
+                $this->writeln($this->green('   ' . $mount->description));
+                $this->sprintf(
+                    '   <:YEL:>Strength<:DEF:> %d',
+                    $mount->strength,
+                );
+                $this->sprintf(
+            '   <:MAG:>Power<:DEF:> %3s  <:MAG:>Crew<:DEF:> %3s',
+                    $mount->requirements->power,
+                    $mount->requirements->crew,
+                );
+            }
+        } else {
+            $this->writeln('None', '');
+        }
+         $this->writeln($this->blue('.......................'));
+
+        return parent::output();
+    }
+}
